75998df30d4e58737b58055b30a0ee48cdaf8bd4
Rodrigo Lucas Pereira <rodrigoyleila@gmail.com>
2019-07-21 23:05:10 -0400

    nova tela mi usuario

--- repositories/acceder/acceder.js
+++ repositories/acceder/acceder.js
@@ -1,63 +1,51 @@
 const _ = require('lodash');
 
 // const { findUserBySocialtoken } = require('../user');
-const { createJwtToken } = require('../../utils');
-const { encrypt } = require('../../utils/');
-const { Usuarios,Personas } = require('../../models');
+const {createJwtToken} = require('../../utils');
+const {encrypt} = require('../../utils/');
+const {Usuarios, Personas} = require('../../models');
 
 /**
  * login the user
  */
-module.exports = async (data) => {
-  let user;
+module.exports = async(data) => {
+    let user;
 
-  if (_.get(data, 'email') && _.get(data, 'contrasena')) {
-    const hashContrasena = encrypt(data.contrasena);
-    user = await Usuarios.findOne({where:
-      {c_contrasena: hashContrasena},
-      include: 
-          {
-            model : Personas,
-            as    : 'persona',
+    if (_.get(data, 'email') && _.get(data, 'contrasena')) {
+        const hashContrasena = encrypt(data.contrasena);
+        user = await Usuarios.findOne({
             where: {
-              c_email: data.email.toLowerCase()
-            }
+                c_contrasena: hashContrasena
             },
-        
-      });
+            include: {
+                model: Personas,
+                as: 'persona',
+                where: {
+                    c_email: data
+                        .email
+                        .toLowerCase()
+                }
+            }
+        });
 
-  } 
-  else {
-    throw {
-      status    : 500, 
-      message   : "No Recibimos el email o la contraseña",
-    };
-  }
-  if (!user) {
-    throw {
-      status    : 500, 
-      message   : "Email o Contraseña incorrecta, favor intentar nuevamente",
-    };
-  }
+    } else {
+        throw {status: 500, message: "No Recibimos el email o la contraseña"};
+    }
+    if (!user) {
+        throw {status: 500, message: "Email o Contraseña incorrecta, favor intentar nuevamente"};
+    }
 
-  if(!user.b_activo)
-  {
-    throw {
-      status    : 500, 
-      message   : "Este usuario no esta activo en el sistema",
-    };
-  }
+    if (!user.b_activo) {
+        throw {status: 500, message: "Este usuario no esta activo en el sistema"};
+    }
 
-  const token = createJwtToken({
-    username: user.email,
-    id: user.id,
-  });
-  let usuario = user.dataValues;
-  delete usuario.c_contrasena;
-  delete usuario.createdAt;
-  delete usuario.updatedAt;
-  return {
-    ...usuario,
-    token,
-  };
+    const token = createJwtToken({username: user.email, id: user.id});
+    let usuario = user.dataValues;
+    delete usuario.c_contrasena;
+    delete usuario.createdAt;
+    delete usuario.updatedAt;
+    return {
+        ...usuario,
+        token
+    };
 };
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,85 +1,126 @@
-const { Usuarios }   = require("../models");
-const { getAllUsers } = require('../repositories/usuarios');
-const { encrypt } = require('../utils/');
-const { traduceErrores,checkAccess } = require('../utils/');
+const {Usuarios, Personas} = require("../models");
+const {getAllUsers, getUserByToken} = require('../repositories/usuarios');
+const {encrypt} = require('../utils/');
+const {traduceErrores, checkAccess} = require('../utils/');
 
 module.exports = (app, router) => {
 
-    router.get('/usuarios', async function(context) {  
-        await checkAccess(context.headers.authorization, 'b_administrador');
-        context.body = await getAllUsers(context.query)
+    router
+        .get('/usuarios', async function (context) {
+            await checkAccess(context.headers.authorization, 'b_administrador');
+            context.body = await getAllUsers(context.query)
 
-    });  
+        });
 
-    router.get('/usuarios/:id', async function(context) {  
+    router.get('/usuarios/miUsuario', async function (context) {
+        let user = await getUserByToken(context.headers.authorization);
+        user = user.dataValues;
+        delete user['c_contrasena'];
+        context.body = user;
+
+    });
+
+    router.get('/usuarios/:id', async function (context) {
         await checkAccess(context.headers.authorization, 'b_administrador');
         const id = context.params.id;
-        context.body = await Usuarios.findOne({where:{id}})        
+        context.body = await Usuarios.findOne({where: {
+                id
+            }})
 
-    });  
+    });
 
-    router.post('/usuarios', async function(context) {  
+    router.post('/usuarios', async function (context) {
         await checkAccess(context.headers.authorization, 'b_administrador');
         let datos = context.request.body;
-        if(datos.c_contrasena)
-        {
-            datos = 
-            {
-                ...datos
-                ,c_contrasena: encrypt(datos.c_contrasena)
+        if (datos.c_contrasena) {
+            datos = {
+                ...datos,
+                c_contrasena: encrypt(datos.c_contrasena)
             }
-    
+
         }
         try
         {
             await Usuarios.create(datos);
             context.body = datos;
-        }
-        catch(error) {
+        } catch (error) {
             throw Error(traduceErrores(error))
         };
-    });  
+    });
 
-    router.put('/usuarios', async function(context) { 
+    router.put('/usuarios', async function (context) {
         await checkAccess(context.headers.authorization, 'b_administrador');
         let datos = context.request.body;
         const id = datos.id;
-        if(id == 1)
-        {
+        if (id == 1) {
             throw Error("Usuario del sistema no se permite actualizar");
         }
-        if(datos.c_contrasena)
-        {
-            datos = 
-            {
-                ...datos
-                ,c_contrasena: encrypt(datos.c_contrasena)
+        if (datos.c_contrasena) {
+            datos = {
+                ...datos,
+                c_contrasena: encrypt(datos.c_contrasena)
             }
-    
+
         }
         try
         {
-            await Usuarios.update( datos , { where: { id } });
+            await Usuarios.update(datos, {where: {
+                    id
+                }});
             context.body = datos;
-        }
-        catch(error) {
+        } catch (error) {
             throw Error(traduceErrores(error))
         };
-    });    
+    });
 
-    router.delete('/usuarios', async function(context) {  
+    router.put('/usuarios/miUsuario', async function (context) {
+        let user = await getUserByToken(context.headers.authorization);
+        let datos = context.request.body;
+        const id = datos.id;
+        if (id != user.id) {
+            throw Error("No se permite actualizar datos de otros usuarios");
+        }
+        if (datos.c_contrasena) {
+            datos = {
+                ...datos,
+                c_contrasena: encrypt(datos.c_contrasena)
+            }
+
+        }
+        delete datos['n_id_persona'];
+        try
+        {
+            await Usuarios.update(datos, {where: {
+                    id
+                }});
+            context.body = await Usuarios.findOne({
+                where: {
+                    id
+                },
+                include: {
+                    model: Personas,
+                    as: 'persona'
+                }
+            })
+        } catch (error) {
+            console.log(error)
+            throw Error(traduceErrores(error))
+        };
+    });
+
+    router.delete('/usuarios', async function (context) {
         await checkAccess(context.headers.authorization, 'b_administrador');
         const {id} = context.query;
-        if(id == 1)
-        {
+        if (id == 1) {
             throw Error("Usuario del sistema no se permite eliminar");
         }
-        try{
-            context.body =  await Usuarios.destroy( { where: { id } });
-        }
-        catch(error) {
+        try {
+            context.body = await Usuarios.destroy({where: {
+                    id
+                }});
+        } catch (error) {
             throw Error(traduceErrores(error))
         };
-    });    
+    });
 
 };
