950c5b37c78acfde57d4d1a9f6d5976a72c7816d
Rodrigo Lucas Pereira <rodrigoyleila@gmail.com>
2019-11-21 09:27:01 -0300

    baixei para 1% o aumento da cotizacao

--- repositories/cotizaciones/guardaCotizaciones.js
+++ repositories/cotizaciones/guardaCotizaciones.js
@@ -6,7 +6,7 @@ module.exports = async(monedas) => {
         //console.log(monedas[moneda],'antes');
         dados = {
             c_monedaOrigemDestino: moneda,
-            n_valor: monedas[moneda] * 1.03
+            n_valor: monedas[moneda] * 1.01
         };
         //console.log(dados.n_valor,'depois');
         await Cotizaciones.create(dados);
