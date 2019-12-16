1a0335a4d809b0a459d6ece0a38bee159fa8a07c
Rodrigo Lucas Pereira <rodrigoyleila@gmail.com>
2019-12-16 09:46:08 -0300

    solucao definitiva para o calculo do valor do comicionisa e da empresa

--- repositories/presupuestos/comisionPresupuestos.js
+++ repositories/presupuestos/comisionPresupuestos.js
@@ -1,122 +1,136 @@
-const {Presupuestos, Pagos, Monedas, Personas, MediosPagos} = require("../../models");
-const sequelize = require('sequelize');
-const {Op} = sequelize;
+const {
+  Presupuestos,
+  Pagos,
+  Monedas,
+  Personas,
+  MediosPagos
+} = require("../../models");
+const sequelize = require("sequelize");
+const { Op } = sequelize;
 
-module.exports = async(query) => {
+module.exports = async query => {
+  const {
+    n_id_presupuesto,
+    n_id_persona_comisionista,
+    d_fecha_ini,
+    d_fecha_fin
+  } = query;
 
-    const {n_id_persona_comisionista, d_fecha_ini, d_fecha_fin} = query;
-
-    let whereComisionista = {
-        id: {
-            [Op.gte]: 1
-        }
+  let whereComisionista = {
+    id: {
+      [Op.gte]: 1
     }
+  };
 
-    if (n_id_persona_comisionista) {
-        whereComisionista = {
-            id: n_id_persona_comisionista
-        }
-    }
+  if (n_id_persona_comisionista) {
+    whereComisionista = {
+      id: n_id_persona_comisionista
+    };
+  }
 
-    const include = [
+  const include = [
+    {
+      model: Monedas,
+      as: "moneda",
+      attributes: ["c_descripcion", "c_simbolo", "c_letras", "n_decimales"]
+    },
+    {
+      model: MediosPagos,
+      as: "medioPago",
+      attributes: ["c_descripcion"]
+    },
+    {
+      model: Personas,
+      as: "personaBaja",
+      attributes: ["c_nombre"]
+    },
+    {
+      model: Presupuestos,
+      as: "presupuesto",
+      include: [
         {
-            model: Monedas,
-            as: 'moneda',
-            attributes: ['c_descripcion', 'c_simbolo', 'c_letras', 'n_decimales']
-        }, {
-            model: MediosPagos,
-            as: 'medioPago',
-            attributes: ['c_descripcion']
-
-        }, {
-            model: Personas,
-            as: 'personaBaja',
-            attributes: ['c_nombre']
-
-        }, {
-            model: Presupuestos,
-            as: 'presupuesto',
-            include: [
-                {
-                    model: Personas,
-                    as: 'comisionista',
-                    attributes: ['c_nombre'],
-                    where: whereComisionista
-
-                }, {
-                    model: Personas,
-                    as: 'persona',
-                    attributes: ['c_nombre', 'c_identificacion']
-
-                }
-            ],
-            attributes: ['n_valor_comision', 'createdAt']
-
+          model: Personas,
+          as: "comisionista",
+          attributes: ["c_nombre"],
+          where: whereComisionista
+        },
+        {
+          model: Personas,
+          as: "persona",
+          attributes: ["c_nombre", "c_identificacion"]
         }
-    ];
-
-    let where = {
-        d_fecha_pago: {
-            [Op.not]: null
-        }
+      ],
+      attributes: ["n_valor_comision", "createdAt"]
     }
-    if (d_fecha_ini) {
-        where = {
-            ...where,
-            d_fecha_pago: {
-                [Op.gte]: d_fecha_ini
-            }
-        }
+  ];
 
+  let where = {
+    d_fecha_pago: {
+      [Op.not]: null
     }
-    if (d_fecha_fin) {
-        where = {
-            ...where,
-            d_fecha_pago: {
-                [Op.lte]: d_fecha_fin
-            }
-        }
+  };
+  if (d_fecha_ini) {
+    where = {
+      ...where,
+      d_fecha_pago: {
+        [Op.gte]: d_fecha_ini
+      }
+    };
+  }
+  if (d_fecha_fin) {
+    where = {
+      ...where,
+      d_fecha_pago: {
+        [Op.lte]: d_fecha_fin
+      }
+    };
+  }
+  if (n_id_presupuesto) {
+    where = {
+      ...where,
+      n_id_presupuesto
+    };
+  }
 
-    }
-    return await Pagos.findAll({
-        where,
-        include,
-        group: [
-            'n_id_presupuesto', sequelize.col('Pagos.createdAt'),
-            sequelize.col('moneda.id'),
-            sequelize.col('presupuesto.id'),
-            sequelize.col('presupuesto->comisionista.id'),
-            sequelize.col('presupuesto->persona.id'),
-            sequelize.col('medioPago.id'),
-            sequelize.col('personaBaja.id')
-        ],
-        attributes: [
-            'n_id_presupuesto',
-            'createdAt',
-            [
-                sequelize.literal(`SUM("Pagos"."n_valor")+SUM("Pagos"."n_desc_redondeo")`),
-                'n_valor_cobrado'
-            ],
-            [
-                sequelize.literal(`SUM("Pagos"."n_valor")`),
-                'n_valor_presupuesto'
-            ],
-            [
-                sequelize.literal(`(presupuesto.n_valor_comision+SUM("Pagos"."n_desc_redondeo"))*(presupuesto.n_porc_comisionista/100)`),
-                'n_valor_comisionista'
-            ],
-            [
-                sequelize.literal(`(presupuesto.n_valor_comision+SUM("Pagos"."n_desc_redondeo")) - ((presupuesto.n_valor_comision+SUM("Pagos"."n_desc_redondeo"))*(presupuesto.n_porc_comisionista/100))`),
-                'n_ganacia_empresa'
-            ]
-        ],
-        order: [
-            [
-                sequelize.col('moneda.id'),
-                'asc'
-            ],
-            ['n_id_presupuesto', 'desc']
-        ]
-    });
-
-}
+  return await Pagos.findAll({
+    where,
+    include,
+    group: [
+      "n_id_presupuesto",
+      sequelize.col("Pagos.createdAt"),
+      sequelize.col("moneda.id"),
+      sequelize.col("presupuesto.id"),
+      sequelize.col("presupuesto->comisionista.id"),
+      sequelize.col("presupuesto->persona.id"),
+      sequelize.col("medioPago.id"),
+      sequelize.col("personaBaja.id")
+    ],
+    attributes: [
+      "n_id_presupuesto",
+      "createdAt",
+      [
+        sequelize.literal(
+          `SUM("Pagos"."n_valor")+SUM("Pagos"."n_desc_redondeo")`
+        ),
+        "n_valor_cobrado"
+      ],
+      [sequelize.literal(`SUM("Pagos"."n_valor")`), "n_valor_presupuesto"],
+      [
+        sequelize.literal(
+          `(presupuesto.n_valor_comision+SUM("Pagos"."n_desc_redondeo"))*(presupuesto.n_porc_comisionista/100)`
+        ),
+        "n_valor_comisionista"
+      ],
+      [
+        sequelize.literal(
+          `(presupuesto.n_valor_comision+SUM("Pagos"."n_desc_redondeo")) - ((presupuesto.n_valor_comision+SUM("Pagos"."n_desc_redondeo"))*(presupuesto.n_porc_comisionista/100))`
+        ),
+        "n_ganacia_empresa"
+      ]
+    ],
+    order: [
+      [sequelize.col("moneda.id"), "asc"],
+      ["n_id_presupuesto", "desc"]
+    ]
+  });
+};
